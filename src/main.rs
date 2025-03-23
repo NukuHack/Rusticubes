87037afac6a2502430b6a5035372d18667eb3183
nukuhack <nukuhack@gmail.com>
2025-03-23 11:24:36 +0100

    image overlay on the triangles

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,13 @@
 use std::io::{stdout, Write};
 use test_app::run;
 use std::env;
-//use std::cmp::max;
-
-struct Point{
-    x: i32,
-    y: i32,
-}
-struct Player {
-    name: String,
-    position: Point,
-}
-impl Player {
-    fn log_pos(&self){
-        print!("player pos: {};", self.position.x);
-        println!("{}", self.position.y);
-    }
-}
-fn make_player() -> Player {
-    Player{name: String::from("Hero"), position:Point{x:0,y:0}}
-}
+use std::cmp::*;
 
 fn main() {
-
     initialize();
 
     let mut lock = stdout().lock();
-    let mut player:Player = make_player();
-    write!(lock, "Begin code:\n{}\n\n", player.name).unwrap();
+    write!(lock, "Begin code:\n\n\n").unwrap();
 
     run_app();
 
